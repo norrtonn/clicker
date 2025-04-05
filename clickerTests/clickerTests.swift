18d2887ea2f250330ac0abe7c4026cdf117cd64f
Norton <baltika1932@mail.ru>
2025-04-05 08:19:10 +0400

    Initial Commit

---
+++ clickerTests/clickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  clickerTests.swift
+//  clickerTests
+//
+//  Created by Николай on 05.04.2025.
+//
+
+import Testing
+@testable import clicker
+
+struct clickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
